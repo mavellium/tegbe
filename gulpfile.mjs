f3fba91f7cb548ff3d964dcaeb7d2589ff55755f
Marcio <marcio.piva@eficazmarketing.com>
2025-06-14 23:19:00 -0300

    Fix: build vercel

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,11 +2,8 @@ import gulp from 'gulp';
 import cleanCSS from 'gulp-clean-css';
 import uglify from 'gulp-uglify';
 import htmlmin from 'gulp-htmlmin';
-import fs from 'fs';
-import path from 'path';
-import globPkg from 'glob';
-const { glob } = globPkg;
 const { src, dest, series, watch } = gulp;
+import imagemin from 'gulp-imagemin';
 
 export function minifyCss() {
   return src('src/css/*.css')
@@ -26,17 +23,18 @@ export function minifyHtml() {
     .pipe(dest('dist'));
 }
 
+
 export function copyImages() {
-  return src('src/assets/**/*.{png,jpg,jpeg,gif,svg}', { allowEmpty: true })
+  return src('src/assets/**/*.{png,jpg,jpeg,gif,svg}', { allowEmpty: true, encoding: false })
     .pipe(dest('dist/assets'));
 }
 
 export function watchFiles() {
-  watch('src/css/*.css', minifyCss);
   watch('src/js/*.js', minifyJs);
   watch('src/*.html', minifyHtml);
-  watch('public/**/*.{png,jpg,jpeg,gif,svg}', copyImages);
+  watch('src/assets/**/*.{png,jpg,jpeg,gif,svg}', copyImages);
 }
 
+  watch('src/css/*.css', minifyCss);
 export const build = series(minifyCss, minifyJs, minifyHtml, copyImages);
 export default series(minifyCss, minifyJs, minifyHtml, copyImages, watchFiles);
