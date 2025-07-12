0e37a1e0f3c187ce00055f2a006600e0241df964
viniciustmota <motaviny140@gmail.com>
2025-07-12 10:42:18 -0300

    feat: carregando video e formulario so quando precisar

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var formContainer = document.getElementById('section-2-form');
+    if (!formContainer) return;
+
+    var observer = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          // Cria os scripts do formulário
+          var scriptInline = document.createElement('script');
+          scriptInline.defer = true;
+          scriptInline.textContent = `!function(a,m,o,c,r,m){a[o+c]=a[o+c]||{setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1529739",hash:"5863b79f491adc496a89844c0bdb543d",locale:"pt"}),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");`;
+
+          var scriptSrc = document.createElement('script');
+          scriptSrc.async = true;
+          scriptSrc.charset = 'utf-8';
+          scriptSrc.src = "https://forms.kommo.com/forms/assets/js/amoforms.js?1750445641";
+
+          formContainer.appendChild(scriptInline);
+          formContainer.appendChild(scriptSrc);
+
+          observer.unobserve(formContainer);
+        }
+      });
+    });
+
+    observer.observe(formContainer);
+  });
+
+document.getElementById('yt-placeholder-main').addEventListener('click', function() {
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute('src', 'https://www.youtube.com/embed/1MIjs_OA-Uc?autoplay=1&rel=0');
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+  iframe.setAttribute('allowfullscreen', '');
+  iframe.style.width = '100%';
+  iframe.style.height = '366px';
+  this.parentNode.replaceChild(iframe, this);
+});
+
+document.querySelectorAll('.yt-placeholder-carousel').forEach(function(placeholder) {
+  placeholder.addEventListener('click', function() {
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', 'https://www.youtube.com/embed/1MIjs_OA-Uc?autoplay=1&rel=0');
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.style.width = '100%';
+    iframe.style.height = '266px';
+    this.parentNode.replaceChild(iframe, this);
+  });
+});
+
+
 function toggleAnswer(element) {
   const answer = element.querySelector(".faq-answer");
   const isVisible = answer.style.display === "block";
